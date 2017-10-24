84ae09febbeb379b9ccb2bc6eb63676196266483
lwang <lwang057@163.com>
2017-10-25 00:07:35 +0800

    Initial commit

---
+++ app/src/main/java/com/lwang/keyboard/MainActivity.java
@@ -0,0 +1,73 @@
+package com.lwang.keyboard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.ziyeyouhu.library.KeyboardTouchListener;
+import com.ziyeyouhu.library.KeyboardUtil;
+
+import static com.lwang.keyboard.R.id.et_access;
+import static com.lwang.keyboard.R.id.scroll;
+
+public class MainActivity extends AppCompatActivity {
+
+    private KeyboardUtil keyboardUtil;
+    private LinearLayout parent;
+    private ScrollView scroll;
+    private EditText etPwd;
+    private EditText etAccess;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        parent= (LinearLayout) findViewById(R.id.parent);
+        scroll= (ScrollView) findViewById(R.id.scroll);
+        etAccess= (EditText) findViewById(R.id.et_access);
+        etPwd= (EditText) findViewById(R.id.et_pwd);
+        initKeyboard();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+
+            if (keyboardUtil != null && keyboardUtil.isShow) {
+
+                keyboardUtil.hideSystemKeyBoard();
+                keyboardUtil.hideAllKeyBoard();
+                keyboardUtil.hideKeyboardLayout();
+            } else {
+
+                return super.onKeyDown(keyCode, event);
+            }
+            return false;
+
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+    private void initKeyboard() {
+        keyboardUtil = new KeyboardUtil(this, parent, scroll);
+        keyboardUtil.setKeyBoardStateChangeListener((i, editText) -> {
+        });
+        keyboardUtil.setInputOverListener((i, editText) -> {
+        });
+        keyboardUtil.setOtherEdittext(etAccess);
+        keyboardUtil.setKeyBoardCursorNew(etPwd);
+        etPwd.setOnTouchListener(new KeyboardTouchListener(keyboardUtil, KeyboardUtil.INPUTTYPE_ABC, -1));
+
+    }
+
+
+
+
+}
---
+++ keyboard/src/main/java/com/ziyeyouhu/library/SIZE.java
@@ -0,0 +1,10 @@
+package com.ziyeyouhu.library;
+
+public class SIZE {
+
+    public final static float KEYBOARY_H = 0.391f;
+    public final static float KEYBOARY_T_H = 0.053f;
+//    public final static float KEYBOARY_H = 0.291f;
+//    public final static float KEYBOARY_T_H = 0.043f;
+
+}
